f29bf5b2edca80ac34c8688cb5b18a5689f8b205
ritcheeze <ritchecereno15@gmail.com>
2026-05-19 11:54:21 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const loginForm = document.querySelector(".login-form");
+const registerForm = document.querySelector(".register-form");
+
+function showRegister(){
+  loginForm.style.display = "none";
+  registerForm.style.display = "block";
+}
+
+function showLogin(){
+  registerForm.style.display = "none";
+  loginForm.style.display = "block";
+}
+const sections = document.querySelectorAll(".section");
+
+function revealSections() {
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+
+        if(sectionTop < window.innerHeight - 100){
+            section.classList.add("show");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealSections);
+window.addEventListener("load", revealSections);
